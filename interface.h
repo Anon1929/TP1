3f9dd942d39607cc7fa00409ce407d95be20b6c1
Marcelo <marcelojunqueiraf@gmail.com>
2021-10-11 22:01:43 -0300

    correções no interface.h

--- interface.h
+++ interface.h
@@ -18,14 +18,14 @@ class IServicoPecaSessaoSala;
 //Autenticacao
 class IApresentacaoAutenticacao {
     public:
-        virtual int autenticar(const matricula&) = 0;
+        virtual int autenticar(const Matricula&) = 0;
         virtual void setCntrServicoAutenticacao(IServicoAutenticacao*) = 0;
         virtual ~IApresentacaoAutenticacao(){}
 };
 
 class IServicoAutenticacao {
     public:
-        virtual int autenticar(const matricula&, const senha&) = 0;
+        virtual int autenticar(const Matricula&, const Senha&) = 0;
         virtual ~IServicoAutenticacao(){}
 };
 
@@ -35,24 +35,24 @@ class IServicoAutenticacao {
 class IApresentacaoParticipante {
     public:
         virtual void cadastrar() = 0;
-        virtual void executar(const matricula&) = 0;
+        virtual void executar(const Matricula&) = 0;
         virtual void setCntrServicoAutenticacao(IServicoAutenticacao*) = 0;
         virtual ~IApresentacaoParticipante(){}
 };
 
 class IServicoParticipante {
     public:
-        virtual int cadastrarUsuario(const participante&) = 0;
-        virtual int editarUsuario(const participante&) = 0;
-        virtual int excluirUsuario(const matricula&) = 0;
-        virtual int cadastrarComoParticipante(const matricula&, const idPeca&) = 0;
+        virtual int cadastrarUsuario(const Participante&) = 0;
+        virtual int editarUsuario(const Participante&) = 0;
+        virtual int excluirUsuario(const Matricula&) = 0;
+        virtual int cadastrarComoParticipante(const Matricula&, const Codigo&) = 0;
         virtual ~IServicoParticipante(){}
 };
 //Peca Sessao Sala
 class IApresentacaoPSS {
     public:
         virtual void executar() = 0;
-        virtual void executar(const matricula&) = 0;
+        virtual void executar(const Matricula&) = 0;
         virtual void setCntrServicoAutenticacao(IServicoAutenticacao*) = 0;
         virtual ~IApresentacaoPSS(){}
 };
@@ -65,20 +65,20 @@ class IServicoPSS {
         virtual vector<Sala> listarSalas() = 0;
         //Com cadastro
         //Peças
-        virtual int incluirPeca (const peca&) = 0;
-        virtual int editarPeca (const peca&) = 0;
-        virtual int excluirPeca (const id&) = 0;
-        virtual int visualizarPeca (const id&, peca*) = 0;
+        virtual int incluirPeca (const Peca&) = 0;
+        virtual int editarPeca (const Peca&) = 0;
+        virtual int excluirPeca (const Codigo&) = 0;
+        virtual int visualizarPeca (const Codigo&, Peca*) = 0;
         //Sessões
-        virtual int incluirSessao (const sessao&) = 0;
-        virtual int editarSessao (const sessao&) = 0;
-        virtual int excluirSessao (const id&) = 0;
-        virtual int visualizarSessao (const id&, sessao*) = 0;
+        virtual int incluirSessao (const Sessao&) = 0;
+        virtual int editarSessao (const Sessao&) = 0;
+        virtual int excluirSessao (const Codigo&) = 0;
+        virtual int visualizarSessao (const Codigo&, Sessao*) = 0;
         //Salas
-        virtual int incluirSala (const sala&) = 0;
-        virtual int editarSala (const sala&) = 0;
-        virtual int excluirSala (const id&) = 0;
-        virtual int visualizarSala (const id&, sala*) = 0;
+        virtual int incluirSala (const Sala&) = 0;
+        virtual int editarSala (const Sala&) = 0;
+        virtual int excluirSala (const Codigo&) = 0;
+        virtual int visualizarSala (const Codigo&, Sala*) = 0;
 
         virtual ~IServicoPSS(){}
 };
